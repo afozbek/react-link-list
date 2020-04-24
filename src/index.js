69c7a4104f117d446d9dc59fdc7cb7e4b0bb848c
afozbek <furkanozbek1995@gmail.com>
2020-04-24 11:34:56 +0300

    npm kullanıldı

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { Provider } from "react-redux";
-import store from "./store/configureStore";
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
