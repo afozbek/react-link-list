2e8ee09b3c8756eb1e019c21c816d6808d0e6990
afozbek <furkanozbek1995@gmail.com>
2020-04-25 14:23:09 +0300

    util fonksiyonlar eklendi

--- src/util/index.js
+++ src/util/index.js
@@ -8,3 +8,25 @@ export const keyCodes = {
   down: 40,
   delete: 46,
 };
+
+export const FILTER_TYPES = {
+  MOST_VOTED: 'MOST_VOTED',
+  LESS_VOTED: 'LESS_VOTED',
+};
+
+export const isValidURL = (str) => {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!pattern.test(str);
+};
+
+export const haveEnoughCharacters = (str, length) => {
+  return str.trim().length >= length;
+};
